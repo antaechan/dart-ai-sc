1cb941db8a671c1201d54a01999c54afb6d077dd
antaechan <0627antaechan@gmail.com>
2025-07-31 19:46:24 +0900

    Add: use disclosure_summary data

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import pandas as pd
 
 def main():
     # 1) 모델/토크나이저 로드
@@ -11,15 +12,21 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)                                  # 모델 이동
     print(f"Using device: {device}")
+    
+    events = pd.read_csv("../data/disclosure_events.csv")
+    sentences = events[events["summary_kr"].notna()]["summary_kr"].tolist()
+    
+     
+    
 
-    # 2) 금융 관련 mock 문장 정의
-    sentences = [
-        "삼성전자의 2분기 실적이 시장 기대치를 상회했다.",
-        "세계 경기 침체 우려로 국내 증시가 약세를 보이고 있다.", 
-        "현대자동차는 전기차 라인업 확대 계획을 발표했다.",
-        "원/달러 환율 급등으로 수입 기업들의 비용 부담이 커졌다.",
-        "카카오페이는 신규 서비스 출시 소식에 주가가 상승했다."
-    ]
+    # # 2) 금융 관련 mock 문장 정의
+    # sentences = [
+    #     "삼성전자의 2분기 실적이 시장 기대치를 상회했다.",
+    #     "세계 경기 침체 우려로 국내 증시가 약세를 보이고 있다.", 
+    #     "현대자동차는 전기차 라인업 확대 계획을 발표했다.",
+    #     "원/달러 환율 급등으로 수입 기업들의 비용 부담이 커졌다.",
+    #     "카카오페이는 신규 서비스 출시 소식에 주가가 상승했다."
+    # ]
 
     # 3) 전처리 ‑ 토큰화
     batch = tokenizer(
